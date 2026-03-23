3c45c7a07228ceced3ddbf9f2c760ae0207d7bd0
onstayrd-afk <onstayrd@gmail.com>
2026-03-23 18:17:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("year").textContent = new Date().getFullYear();
+
+document.getElementById("btnDemo").addEventListener("click", function () {
+  window.location.href = "#contacto";
+});
